aa7c276179541305b5b128a1f19fcbd1d1d613bf
noprizal <noprizal@voltras.co.id>
2024-06-05 15:20:07 +0700

    setup product page

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const electron = require("electron");
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain, screen } = electron;
 
 let mainWindow;
+let productWindow;
 
 const mainWin = () => {
   mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false
     },
-    // width: 800, 
     height: 550,
     autoHideMenuBar: true,
     title: "My POS 1.0.0",
@@ -33,3 +34,33 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+//communication with rendererProcess (front end)
+ipcMain.on("load:product-window", () => {
+  productWin()
+})
+
+const productWin = () => {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  productWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    autoHideMenuBar: true,
+    width,
+    height,
+    title: "My POS | Product"
+  })
+  // productWindow.webContents.openDevTools();
+  productWindow.loadFile("windows/product.html");
+
+  productWindow.webContents.on("did-finish-load", () => {
+    mainWindow.hide();
+  })
+  productWindow.on("close", () => {
+    mainWindow.show();
+  })
+
+}
