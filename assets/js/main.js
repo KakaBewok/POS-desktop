2494536a132923f013a80c8316c8fb5da0c38566
noprizal <noprizal@voltras.co.id>
2024-06-17 01:01:42 +0700

    ngeri

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,28 @@ const loadData = () => {
 };
 
 loadData();
+
+const deleteRecord = (id) => {
+  let table;
+  switch (docId) {
+    case "product-data":
+      table = `product`;
+      break;
+  }
+  const queryDelete = `delete from ${table} where id = ${id}`;
+
+  db.run(queryDelete, (err) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      dialog.showMessageBoxSync({
+        title: "Alert",
+        message: "Deleted",
+        type: "info",
+        buttons: ["OK"],
+      });
+      loadData();
+    }
+  });
+};
--- assets/js/toolbar.js
+++ assets/js/toolbar.js
@@ -10,3 +10,24 @@ const closeFormAddData = () => {
   $("#form-add-data").addClass("hidden");
   $("#table-separator").addClass("hidden");
 };
+
+const deleteAction = (id = null, producName = null) => {
+  const message = `Are you sure, you want to delete data "${producName}" ?`;
+
+  if (id) {
+    let dialogBox = dialog.showMessageBoxSync({
+      title: "Delete record",
+      type: "question",
+      buttons: ["Yes", "No"],
+      defaultId: 0,
+      message,
+      detail: "This action cannot be undone.",
+    });
+    if (dialogBox === 1) {
+      $("input.data-checkbox").prop("checked", false);
+      $("tbody#data tr").removeClass("blocked");
+    } else {
+      deleteRecord(id);
+    }
+  }
+};
